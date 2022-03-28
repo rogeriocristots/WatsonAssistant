23731d1b8a7720981a20f67018c0635e5eae8628
rogeriocristots <83675300+rogeriocristots@users.noreply.github.com>
2022-03-28 17:23:20 +0100

    Update server.js

--- Weather-API/server.js
+++ Weather-API/server.js
@@ -1,31 +1,148 @@
-const http = require('http');
-const options = {
-    uri:
-      "https://api.openweathermap.org/data/2.5/weather?lat=38.736946&lon=-9.142685&appid=0f2eb9a2feb9bbc47282ab9340253b05",
-    json: true,
-  };
+/**
+ *
+ * main() will be run when you invoke this action
+ *
+ * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+ *
+ * @return The output of this action, which must be a JSON object.
+ *
+ */
+var request = require("request-promise");
 
-let rp = require("request-promise");
+const DiscoveryV1 = require('ibm-watson/discovery/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
 
-rp(options).then((res) => {
-    let WeatherReport =
-      "Current Temperature : " +
-      res.main.temp +
-      ",</br>Pressure : " +
-      res.main.pressure +
-      ",</br>Humidity : " +
-      res.main.humidity +
-      ",</br>Temp min : " +
-      res.main.temp_min +
-      " ,</br>Temp max : " +
-      res.main.temp_max;
-    return { message: WeatherReport };
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
-http.createServer(function (request, response) {
-   target = process.env.TARGET ? process.env.TARGET : 'World' ;
-   msg = process.env.MSG ? process.env.MSG : 'Weather from server.js\n' + WeatherReport + '\n';
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end(msg);
-}).listen(8080);
+async function main(params) {
+    
+    // Se for para consultar uma das APIs
+     if (params.type === "api") {
+        try {
+            //convert currency EUR to USD OU RUSD
+            if (params.operation == "convertcurrency"){
+                const urlParams = params.moedaorigem + `-` + params.moedaconversao;
+                const url = `https://economia.awesomeapi.com.br/last/${urlParams}`;  
+                
+                const summary = await request({
+                    method: "GET",
+                    uri: url,
+                    json: true,
+                 });
+                 
+                const position = params.moedaorigem + params.moedaconversao;
+                const moedavalor = summary[position].bid;
+                const calcMoeda = params.quantidademoeda * moedavalor;
+        
+                    return {
+                         result: `Conversão ${summary[position].name}: \n` +
+                         `${params.quantidademoeda} ${params.moedaorigem} representam ${calcMoeda} ${params.moedaconversao} \n` +
+                         `Fonte: Awesome API - Economia`,
+                    };
+            } 
+            // check covid por concelho
+            else if (params.operation == "statuscovid") {
+                
+               if (params.location) {
+                 // Obtem lista de Concelhos  
+                 const urlCheck = `https://covid19-api.vost.pt/Requests/get_county_list/`;  
+                   
+                 const locationsList = await request({
+                     method: "GET",
+                     uri: urlCheck,
+                     json: true,
+                  });    
+                  
+                  // Verifica se o concelho inserido se encontra na lista
+                  var count = 0;
+                  
+                  for (var i = 0; i < locationsList.length; i++) {
+                       if ( locationsList[i].toLowerCase() ===  params.location.toLowerCase()) {
+                            count = count + 1;
+                       }
+                  }
+                  
+                  // Se o concelho estiver na lista
+                  if (count > 0){
+                     const url = `https://covid19-api.vost.pt/Requests/get_last_update_specific_county/${params.location}`;  
+                       
+                     const summary = await request({
+                         method: "GET",
+                         uri: url,
+                         json: true,
+                      });
+             
+                         return {
+                              result: `Informações do concelho de ${summary[0].concelho}: \n` +
+                                      `Distrito: ${summary[0].distrito} \n`+ 
+                                      `Nivel de Risco: ${summary[0].incidencia_risco} \n`+ 
+                                      `Casos em 14 dias: ${summary[0].casos_14dias} \n`+ 
+                                      `Informação de : ${summary[0].data}` ,
+                         };
+                  } else {
+                     return {
+                          result: `Não encontrei a localização!`,
+                     };
+                  }
+               }
+            }
+    
+    
+        } catch (err) {
+          return { error: "it failed : " + err };
+        }
+    }
+    // Se for para consultar o discovery
+    else{
 
-console.log('Server running at http://0.0.0.0:8080/');
+        const discovery = new DiscoveryV1({
+            authenticator: new IamAuthenticator({ apikey: params.apikey }),
+            serviceUrl: params.url,
+        	version: '2021-04-22',
+        });
+        
+        const offset = getRandomInt(50);
+        
+        const queryParams = {
+                  environmentId: params.env_id,
+                  collectionId: params.collection_id,
+                  //natural_language_query: params.input,
+                   query: params.inputQuery,
+                  count: params.nrOcc,
+                  offset: offset
+                };
+                
+
+        
+        try {
+            
+          data = await discovery.query(queryParams);
+          let response;
+          
+          
+          if (	params.type === "discoveryInstrucoes") {
+            response = data.result.results.map((v, i) => {
+                return `<b>${v.title}</b>
+                         ${v.text}`;
+             });   
+          } else {
+            response = data.result.results.map((v, i) => {
+                return `<b>${v.question}</b>
+                         ${v.answer}`;
+             });
+          }
+          
+         return {
+            result:
+              "<b>Aqui estão alguns artigos que encontramos. Não podemos verificar a exactidão de todas estas fontes.</b>\n\n" +
+              response.join("\n\n"),
+          };
+
+          
+        } catch (err) {
+          return { error: "it failed : " + err };
+        }
+    }
+}
